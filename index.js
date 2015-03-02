204a2df2db279db562ccfe123287091fcdae7d41
jegtnes <alex.jegtnes@cxpartners.co.uk>
2015-03-02 17:37:22 +0000

    Implemented Spooky boilerplate example to work with first CLI argument as URL

--- index.js
+++ index.js
@@ -6,4 +6,52 @@ program
   .version('0.1.0')
   .parse(process.argv)
 
-console.log(program.args);
+parseURL = program.args[0];
+
+try {
+    var Spooky = require('spooky');
+} catch (e) {
+    var Spooky = require('../lib/spooky');
+}
+
+var spooky = new Spooky({
+  child: {
+    transport: 'http'
+  },
+  casper: {
+    logLevel: 'debug',
+    verbose: true
+  }
+}, function (err) {
+  if (err) {
+    e = new Error('Failed to initialize SpookyJS');
+    e.details = err;
+    throw e;
+  }
+
+  spooky.start(parseURL);
+  spooky.then(function () {
+    this.emit('hello', 'Hello, from ' + this.evaluate(function () {
+      return document.title;
+    }));
+  });
+  spooky.run();
+});
+
+spooky.on('error', function (e, stack) {
+  console.error(e);
+
+  if (stack) {
+    console.log(stack);
+  }
+});
+
+spooky.on('hello', function (greeting) {
+  console.log(greeting);
+});
+
+spooky.on('log', function (log) {
+  if (log.space === 'remote') {
+    console.log(log.message.replace(/ \- .*/, ''));
+  }
+});
